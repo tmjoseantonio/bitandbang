3878720e53e2c4e272516205a7e7bde2bb5808a3
Jose Tovar <tmjoseantonio@gmail.com>
2019-03-13 11:04:05 -0500

    Update card.js
    Update package.json
    
    Update package.json
    
    Update package.json
    
    Update package-lock.json
    
    Update README.md
    
    Update card.js
    
    Update package.json
    
    Update card.js

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Forked from https://github.com/bnb/bitandbang
 # Usage
 Via npx:
 ```
-npx sergio
+npx tovar
 ```
--- bin/card.js
+++ bin/card.js
@@ -14,34 +14,16 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Sergio NS /'),
-  handle: chalk.cyan('sergionsz'),
-  work: chalk.white('Senior Web Engineer at Huge'),
-  twitter: chalk.cyan('https://twitter.com/sergionsz'),
-  github: chalk.cyan('https://github.com/sergionsz'),
-  linkedin: chalk.cyan('https://linkedin.com/in/sergionsz'),
-  // web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx sergio'),
-  labelWork: chalk.white.bold('      Work:'),
-  labelTwitter: chalk.white.bold('   Twitter:'),
-  labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  // labelWeb: chalk.white.bold('       Web:'),
+  npx: chalk.white('npx tovar'),
   labelCard: chalk.white.bold('      Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
-const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-// const webing = `${data.labelWeb}  ${data.web}`
+const heading = `@tmjoseantonio: Better than @sergionsz`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-// const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + newline + newline + carding
+const output = heading + newline + newline + newline + carding
 
 console.log(chalk.green(boxen(output, options)))
